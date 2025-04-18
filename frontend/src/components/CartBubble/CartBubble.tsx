0e26a500d2c9bc42ae5baf4db4cb3ca88324e3dc
Your Name <your.email@example.com>
2025-04-18 15:17:06 -0400

    added cartBubble component for displaying number of items.

---
+++ frontend/src/components/CartBubble/CartBubble.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCartCount } from "@/store/slices/cartSlice"; // Make sure the path is correct
+import "./CartBubble.css";
+
+const CartBubble: React.FC = () => {
+  const cartCount = useSelector(selectCartCount);
+  console.log("CartBubble Rendered, Cart Count: ", cartCount); // Check if it renders correctly
+  return (
+    <div className="cart-icon">
+      {cartCount > 0 && <div className="cart-bubble">{cartCount}</div>}
+    </div>
+  );
+};
+
+export default CartBubble;
