428cc89d2d8315ac2331004e42eb3dd6cdc6db9f
Your Name <your.email@example.com>
2025-04-18 15:15:18 -0400

    Adjusted AttributeItemsResolver

--- backend/src/GraphQL/resolvers/AttributeItemsResolver.php
+++ backend/src/GraphQL/resolvers/AttributeItemsResolver.php
@@ -7,13 +7,8 @@ use PDO;
 class AttributeItemsResolver
 {
 
-    public static function fetchattributes($attribute)
+    public static function fetchattributes($pdo, $attribute)
     {
-        error_log($attribute['id']);
-        $pdo = new PDO("mysql:host=localhost;dbname=scandiweb", "root", "1234", [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-
         $stmt = $pdo->prepare("SELECT * FROM attribute_items WHERE attribute_id = :attribute_id");
         $stmt->bindParam(':attribute_id', $attribute['id'], PDO::PARAM_INT);
         $stmt->execute();
