cea46fcc5c31cd99363a0b110765da0ba4e9d3bb
Your Name <your.email@example.com>
2025-04-18 15:14:56 -0400

    adding loading component and Add to Cart button

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,13 @@
+const Loading = () => {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+  };
+
+  return <div style={containerStyle}>Loading ....</div>;
+};
+
+export default Loading;
