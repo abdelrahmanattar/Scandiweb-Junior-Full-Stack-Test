7a2bde030f36951d18537c68d6f35fbf8f6b240e
Your Name <your.email@example.com>
2025-04-18 15:13:51 -0400

    Redux files and navbarcart icon

---
+++ frontend/src/store/slices/cartSlice.ts
@@ -0,0 +1,78 @@
+// store/cartSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface ProductAttribute {
+  attribute_name: string;
+  attribute_type: "text" | "swatch";
+  attribute_item: { value: string }[];
+}
+interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  attributes: { [key: string]: string };
+  all_attributes: { [key: string]: ProductAttribute}
+  image: string
+}
+
+interface CartState {
+  items: CartItem[];
+}
+
+const initialState: CartState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action: PayloadAction<CartItem>) {
+      const existingItem = state.items.find(
+        item =>
+          item.id === action.payload.id &&
+          JSON.stringify(item.all_attributes) === JSON.stringify(action.payload.all_attributes) &&
+          JSON.stringify(item.attributes) === JSON.stringify(action.payload.attributes)
+      );
+      if (existingItem) {
+        existingItem.quantity += action.payload.quantity;
+      } else {
+        state.items.push(action.payload);
+      }
+    },
+
+    incrementItem(state, action: PayloadAction<{ id: string; attributes: { [key: string]: string } }>) {
+      const item = state.items.find(
+        item =>
+          item.id === action.payload.id &&
+          JSON.stringify(item.attributes) === JSON.stringify(action.payload.attributes)
+      );
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+
+    decrementItem(state, action: PayloadAction<{ id: string; attributes: { [key: string]: string } }>) {
+      const itemIndex = state.items.findIndex(
+        item =>
+          item.id === action.payload.id &&
+          JSON.stringify(item.attributes) === JSON.stringify(action.payload.attributes)
+      );
+      if (itemIndex !== -1) {
+        const item = state.items[itemIndex];
+        if (item.quantity > 1) {
+          item.quantity -= 1;
+        } else {
+          state.items.splice(itemIndex, 1);
+        }
+      }
+    },
+  },
+});
+
+export const selectCartCount = (state: any) =>
+  state.cart.items.reduce((total: any, item: any) => total + item.quantity, 0);
+
+export const { addItem, incrementItem, decrementItem } = cartSlice.actions;
+export default cartSlice.reducer;
---
+++ frontend/src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categoryReducer from './slices/categorySlice'
+import cartReducer from './slices/cartSlice'
+
+export const store = configureStore({
+  reducer: {
+    category: categoryReducer,
+    cart: cartReducer,  
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
